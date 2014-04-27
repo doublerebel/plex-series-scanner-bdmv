d32c52871853cd5dd4dbe081e6e1281b88c8b5d6
Charles Phillips <charles@doublerebel.com>
2014-04-27 15:24:25 -0700

    Strip trailing whitespace

--- Plex Series Scanner (with disc image support).py
+++ Plex Series Scanner (with disc image support).py
@@ -21,7 +21,7 @@ standalone_episode_regexs = [
   '(.*?)( \(([0-9]+)\))? - ([0-9]+)+x([0-9]+)(-[0-9]+[Xx]([0-9]+))?( - (.*))?',  # Newzbin style, no _UNPACK_
   '(.*?)( \(([0-9]+)\))?[Ss]([0-9]+)+[Ee]([0-9]+)(-[0-9]+[Xx]([0-9]+))?( - (.*))?'   # standard s00e00
   ]
-  
+
 season_regex = '.*?(?P<season>[0-9]+)$' # folder for a season
 
 just_episode_regexs = [
@@ -39,23 +39,23 @@ ends_with_episode = ['[ ]*[0-9]{1,2}x[0-9]{1,3}$', '[ ]*S[0-9]+E[0-9]+$']
 
 # Look for episodes.
 def Scan(path, files, mediaList, subdirs, language=None, root=None):
-  
+
   # Scan for video files.
   VideoFilesScan(path, files, mediaList, subdirs, root)
-  
+
   # Take top two as show/season, but require at least the top one.
   paths = Utils.SplitPath(path)
   shouldStack = True
-  
+
   if len(paths) == 1 and len(paths[0]) == 0:
-  
+
     # Run the select regexps we allow at the top level.
     for i in files:
       file = os.path.basename(i)
       for rx in episode_regexps[0:-1]:
         match = re.search(rx, file, re.IGNORECASE)
         if match:
-          
+
           # Extract data.
           show = match.group('show')
           season = int(match.group('season'))
@@ -63,7 +63,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
           endEpisode = episode
           if match.groupdict().has_key('secondEp') and match.group('secondEp'):
             endEpisode = int(match.group('secondEp'))
-          
+
           # Clean title.
           name, year = CleanName(show)
           if len(name) > 0:
@@ -72,36 +72,36 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
               tv_show.display_offset = (ep-episode)*100/(endEpisode-episode+1)
               tv_show.parts.append(i)
               mediaList.append(tv_show)
-  
+
   elif len(paths) > 0 and len(paths[0]) > 0:
     done = False
-        
+
     # See if parent directory is a perfect match (e.g. a directory like "24 - 8x02 - Day 8_ 5_00P.M. - 6_00P.M")
     if len(files) == 1:
       for rx in standalone_episode_regexs:
         res = re.findall(rx, paths[-1])
         if len(res):
           show, junk, year, season, episode, junk, endEpisode, junk, title = res[0]
-          
+
           # If it didn't have a show, then grab it from the directory.
           if len(show) == 0:
             (show, year) = CleanName(paths[0])
-            
+
           episode = int(episode)
           if len(endEpisode) > 0:
             endEpisode = int(endEpisode)
           else:
             endEpisode = episode
-            
+
           for ep in range(episode, endEpisode+1):
             tv_show = Media.Episode(show, season, ep, title, year)
             tv_show.display_offset = (ep-episode)*100/(endEpisode-episode+1)
             tv_show.parts.append(files[0])
             mediaList.append(tv_show)
-            
+
           done = True
           break
-          
+
     if done == False:
 
       # Not a perfect standalone match, so get information from directories. (e.g. "Lost/Season 1/s0101.mkv")
@@ -109,14 +109,14 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
       seasonNumber = None
 
       (show, year) = CleanName(paths[0])
-      
+
       # Which component looks like season?
       if len(paths) >= 2:
         season = paths[len(paths)-1]
         match = re.match(season_regex, season, re.IGNORECASE)
         if match:
           seasonNumber = int(match.group('season'))
-      
+
       # Make sure an episode name didn't make it into the show.
       for rx in ends_with_episode:
         show = re.sub(rx, '', show)
@@ -125,21 +125,21 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
         done = False
         file = os.path.basename(i)
         (file, ext) = os.path.splitext(file)
-        
+
         if ext.lower() in ['.mp4', '.m4v', '.mov']:
           m4season = m4ep = m4year = 0
           m4show = title = ''
-          try: 
+          try:
             mp4fileTags = mp4file.Mp4File(i)
-            
+
             # Show.
             try: m4show = find_data(mp4fileTags, 'moov/udta/meta/ilst/tvshow').encode('utf-8')
             except: pass
-              
+
             # Season.
             try: m4season = int(find_data(mp4fileTags, 'moov/udta/meta/ilst/tvseason'))
             except: pass
-              
+
             # Episode.
             m4ep = None
             try:
@@ -152,7 +152,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
               except:
                 # TV Episode (can be 101)
                 m4ep = int(find_data(mp4fileTags, 'moov/udta/meta/ilst/tvepisode'))
-            
+
             if m4ep is not None:
               found = False
               try:
@@ -163,7 +163,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
                     m4season = int(match.group('season'))
                     m4ep = int(match.group('ep'))
                     found = True
-              
+
                 if found == False and re.match('[0-9]+', str(m4ep)):
                   # Carefully convert to episode number.
                   m4ep = int(m4ep) % 100
@@ -175,11 +175,11 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
             # Title.
             try: title = find_data(mp4fileTags, 'moov/udta/meta/ilst/title').encode('utf-8')
             except: pass
-              
+
             # Year.
             try: m4year = int(find_data(mp4fileTags, 'moov/udta/meta/ilst/year')[:4])
             except: pass
-            
+
             if year and m4year == 0:
               m4year = year
 
@@ -192,7 +192,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
 
           except:
             pass
-        
+
         # Check for date-based regexps first.
         for rx in date_regexps:
           match = re.search(rx, file)
@@ -225,14 +225,14 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
           cleanName, cleanYear = CleanName(file)
           if cleanYear != None:
             file = file.replace(str(cleanYear), 'XXXX')
-            
+
           # Minor cleaning on the file to avoid false matches on H.264, 720p, etc.
           whackRx = ['([hHx][\.]?264)[^0-9]', '[^[0-9](720[pP])', '[^[0-9](1080[pP])', '[^[0-9](480[pP])']
           for rx in whackRx:
             file = re.sub(rx, ' ', file)
-          
+
           for rx in episode_regexps:
-            
+
             match = re.search(rx, file, re.IGNORECASE)
             if match:
               # Parse season and episode.
@@ -241,19 +241,19 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
               endEpisode = episode
               if match.groupdict().has_key('secondEp') and match.group('secondEp'):
                 endEpisode = int(match.group('secondEp'))
-                
+
               # More validation for the weakest regular expression.
               if rx == episode_regexps[-1]:
-                
+
                 # Look like a movie? Skip it.
                 if re.match('.+ \([1-2][0-9]{3}\)', paths[-1]):
                   done = True
                   break
-                  
+
                 # Skip episode 0 on the weak regex since it's pretty much never right.
                 if the_season == 0:
                   break
-                  
+
                 # Make sure this isn't absolute order.
                 if seasonNumber is not None:
                   if seasonNumber != the_season:
@@ -261,7 +261,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
                     if re.search('^[0-9]+[ -]', file):
                       # Let the episode matcher have it.
                       break
-                    
+
                     # Treat the whole thing as an episode.
                     episode = episode + the_season*100
                     if endEpisode is not None:
@@ -272,12 +272,12 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
                 tv_show.display_offset = (ep-episode)*100/(endEpisode-episode+1)
                 tv_show.parts.append(i)
                 mediaList.append(tv_show)
-              
+
               done = True
               break
-              
+
         if done == False:
-          
+
           # OK, next let's see if we're dealing with something that looks like an episode.
           # Begin by cleaning the filename to remove garbage like "h.264" that could throw
           # things off.
@@ -285,7 +285,7 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
           (file, fileYear) = CleanName(file)
 
           # if don't have a good year from before (when checking the parent folders) AND we just got a good year, use it.
-          if not year and fileYear: 
+          if not year and fileYear:
             year = fileYear
 
           for rx in just_episode_regexs:
@@ -293,11 +293,11 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
             if episode_match is not None:
               the_episode = int(episode_match.group('ep'))
               the_season = 1
-              
+
               # Now look for a season.
               if seasonNumber is not None:
                 the_season = seasonNumber
-                
+
                 # See if we accidentally parsed the episode as season.
                 if the_episode >= 100 and int(the_episode / 100) == the_season:
                   the_episode = the_episode % 100
@@ -305,20 +305,20 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
               # Prevent standalone eps matching the "XX of YY" regex from stacking.
               if rx == just_episode_regexs[0]:
                 shouldStack = False
-              
+
               tv_show = Media.Episode(show, the_season, the_episode, None, year)
               tv_show.parts.append(i)
               mediaList.append(tv_show)
               done = True
               break
-          
+
         if done == False:
           print "Got nothing for:", file
-          
+
   # Stack the results.
   if shouldStack:
     Stack.Scan(path, files, mediaList, subdirs)
-  
+
 def find_data(atom, name):
   child = atomsearch.find_path(atom, name)
   data_atom = child.find('data')
@@ -326,7 +326,7 @@ def find_data(atom, name):
     return data_atom.attrs['data']
 
 import sys
-    
+
 if __name__ == '__main__':
   print "Hello, world!"
   path = sys.argv[1]
@@ -336,7 +336,7 @@ if __name__ == '__main__':
   print "Media:", media
 
 
-video_exts = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bin', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv', 'ifo', 'img', 
+video_exts = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bin', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv', 'ifo', 'img',
               'iso', 'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nrg', 'nsv', 'nuv', 'ogm', 'ogv', 'tp',
               'pva', 'qt', 'rm', 'rmvb', 'sdp', 'svq3', 'strm', 'ts', 'ty', 'vdr', 'viv', 'vob', 'vp3', 'wmv', 'wpl', 'wtv', 'xsp', 'xvid', 'webm']
 
@@ -363,7 +363,7 @@ yearRx = '([\(\[\.\-])([1-2][0-9]{3})([\.\-\)\]_,+])'
 
 # Cleanup folder / filenames
 def CleanName(name):
-  
+
   orig = name
 
   # Make sure we pre-compose.  Try to decode with reported filesystem encoding, then with UTF-8 since some filesystems lie.
@@ -386,28 +386,28 @@ def CleanName(name):
     if yearInt > 1900 and yearInt < (datetime.date.today().year + 1):
       year = int(yearStr)
       name = name.replace(yearMatch.group(1) + yearStr + yearMatch.group(3), ' *yearBreak* ')
-    
+
   # Take out things in brackets. (sub acts weird here, so we have to do it a few times)
   done = False
   while done == False:
     (name, count) = re.subn(r'\[[^\]]+\]', '', name, re.IGNORECASE)
     if count == 0:
       done = True
-  
+
   # Take out bogus suffixes.
   for suffix in ignore_suffixes:
     rx = re.compile(suffix + '$', re.IGNORECASE)
     name = rx.sub('', name)
-  
+
   # Take out audio specs, after suffixing with space to simplify rx.
   name = name + ' '
   for s in audio:
     rx = re.compile(s, re.IGNORECASE)
     name = rx.sub(' ', name)
-  
+
   # Now tokenize.
   tokens = re.split('([^ \-_\.\(\)+]+)', name)
-  
+
   # Process tokens.
   newTokens = []
   for t in tokens:
@@ -426,29 +426,29 @@ def CleanName(name):
   garbage.extend(source)
   garbage.extend(video_exts)
   garbage = set(garbage)
-  
+
   for t in newTokens:
     if t.lower() in garbage:
       tokenBitmap.append(False)
     else:
       tokenBitmap.append(True)
-  
+
   # Now strip out the garbage, with one heuristic; if we encounter 2+ BADs after encountering
   # a GOOD, take out the rest (even if they aren't BAD). Special case for director's cut.
   numGood = 0
   numBad  = 0
-  
+
   finalTokens = []
-  
+
   for i in range(len(tokenBitmap)):
     good = tokenBitmap[i]
-    
+
     # If we've only got one or two tokens, don't whack any, they might be part of
     # the actual name (e.g. "Internal Affairs" "XXX 2")
     #
     if len(tokenBitmap) <= 2:
       good = True
-    
+
     if good and numBad < 2:
       if newTokens[i] == '*yearBreak*':
         #if we have a year, we can ignore everything after this.
@@ -457,7 +457,7 @@ def CleanName(name):
         finalTokens.append(newTokens[i])
     elif not good and newTokens[i].lower() == 'dc':
       finalTokens.append("(Director's cut)")
-    
+
     if good == True:
       numGood += 1
     else:
@@ -466,27 +466,27 @@ def CleanName(name):
   # If we took *all* the tokens out, use the first one, otherwise we'll end up with no name at all.
   if len(finalTokens) == 0 and len(newTokens) > 0:
     finalTokens.append(newTokens[0])
-    
+
   #print "CLEANED [%s] => [%s]" % (orig, u' '.join(finalTokens))
   #print "TOKENS: ", newTokens
   #print "BITMAP: ", tokenBitmap
   #print "FINAL:  ", finalTokens
-  
+
   cleanedName = ' '.join(finalTokens)
   cleanedName = cleanedName.encode('utf-8')
   return (titlecase.titlecase(cleanedName), year)
 
 # Remove files that aren't videos.
 def VideoFilesScan(path, files, mediaList, subdirs, root=None):
-  
+
   # Filter out bad stuff.
   Filter.Scan(path, files, mediaList, subdirs, video_exts, root)
-  
+
   filesToRemove = []
   for i in files:
     # Remove files that aren't video.
     (file, ext) = os.path.splitext(i)
-      
+
     # Remove sample files if they're smaller than 300MB.
     for rx in ignore_samples:
       if re.search(rx, i, re.IGNORECASE) and os.path.getsize(i) < 300 * 1024 * 1024:
@@ -496,17 +496,17 @@ def VideoFilesScan(path, files, mediaList, subdirs, root=None):
     for rx in ignore_trailers:
       if re.search(rx, i, re.IGNORECASE):
         filesToRemove.append(i)
-        
+
   # Uniquify and remove.
   filesToRemove = list(set(filesToRemove))
   for i in filesToRemove:
     files.remove(i)
-      
+
   # Check directories, but not at the top-level.
   ignore_dirs_total = []
   if len(path) > 0:
     ignore_dirs_total += ignore_dirs
-  
+
   whack = []
   for dir in subdirs:
     baseDir = os.path.basename(dir)
@@ -514,17 +514,17 @@ def VideoFilesScan(path, files, mediaList, subdirs, root=None):
       if re.match(rx, baseDir, re.IGNORECASE):
         whack.append(dir)
         break
-  
+
   for w in whack:
     subdirs.remove(w)
-      
+
 def RetrieveSource(name):
   name = os.path.basename(name)
   wordSplitter = re.compile(r"[^a-zA-Z0-9']+", re.IGNORECASE)
   words = wordSplitter.split(name.upper())
   sources = source
   foundSources = []
-  
+
   # Return the first one we find.
   for d in source_dict:
     for s in source_dict[d]:
@@ -541,5 +541,5 @@ def FindYear(words):
       if year > 1900 and year < datetime.date.today().year + 1:
         return i
     i += 1
-    
+
   return None
